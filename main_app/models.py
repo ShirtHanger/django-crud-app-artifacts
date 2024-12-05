238dca507a8bc867f6adcef8fe61223fef593d08
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-12-04 19:50:37 -0600

    Up to date with other app

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Artifact(models.Model): #References model class, new API schema!
+    name = models.CharField(max_length=100)
+    culture = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000) # textField is better for longer text fields
+    year_discovered = models.IntegerField()
+    
+    # Override class object nonsense, just return the cat's name
+    def __str__(self):
+        return self.name
